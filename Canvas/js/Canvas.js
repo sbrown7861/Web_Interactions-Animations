20592f44c58a7c02f110a6dfa574ea6d85b81c82
sbrown7861 <sbrown14@fullsail.edu>
2015-08-05 19:52:13 -0700

    Canvas Video 1
    Follow along with video 1 for canvas. Used external sheets for project
    instead of inline.

---
+++ Canvas/js/Canvas.js
@@ -0,0 +1,13 @@
+/**
+ * Created by skylerbrown on 8/5/15.
+ */
+
+
+window.onload = function(){
+
+    var theCanvas = document.getElementById("Canvas1");
+
+    theCanvas.width= 400;
+    theCanvas.height= 400;
+
+};
